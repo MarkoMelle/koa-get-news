f8e2e0404e8a683a7e4bcb84ffdaae2ddb38315c
Marko Melle <markmelle1@gmail.com>
2023-03-22 22:24:19 +0100

    server.js changed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const delay = koaSlow({ delay: 3000 });
 
 const NEWS_API_KEY = 'e8b58b7078694608825d928f17664614';
 
-app.use(cors());
+
 
 router.get('/news', async (ctx) => {
   try {
@@ -41,7 +41,7 @@ router.get('/news', async (ctx) => {
     ctx.status = 500;
   }
 });
-
+app.use(cors);
 app.use(delay);
 app.use(router.routes());
 
